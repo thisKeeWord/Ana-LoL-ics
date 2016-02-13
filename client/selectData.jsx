305ca7637dd9da3f9e1f5a834194b962fc4e3cf7
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2016-02-12 19:01:18 -0800

    starting new feature

--- client/selectData.jsx
+++ client/selectData.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 class SelectData extends React.Component {
 
-  
+  render() {
+    console.log(this.props.timeline, 'selectData');
+    console.log(this.props.spot, 'selectData');
+    console.log(this.props.playerInfo, 'selectData');
+    return (
+    	<div>
+    	</div>
+    )
+  }
 }
 
 module.exports = SelectData;
