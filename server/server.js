861c507821ca5b80b87e1f1b397ef4745e2856b9
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2016-03-10 15:42:57 -0800

    porting?

--- server/server.js
+++ server/server.js
@@ -5,11 +5,10 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var controller = require('./../Controller/controler.js');
 var stuff = require('./../stuff.js');
-var port = process.env.PORT || 5000;
 var mongoURI = 'mongodb://lkee:' + process.env.stuff3 + '@ds011439.mlab.com:11439/heroku_wk47xfd5';
 mongoose.connect(mongoURI);
 
-
+app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
@@ -25,4 +24,6 @@ app.post('/found', controller.matchList);
 
 app.post('/getGameData', controller.getData);
 
-app.listen(port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
