2782bdebce52804760755a75272d3c544e62c3c2
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2016-03-13 21:04:43 -0700

    CMON BUDDY~

--- server/server.js
+++ server/server.js
@@ -7,14 +7,10 @@ var controller = require('./../Controller/controler.js');
 var mongoURI = 'mongodb://lkee:' + process.env.stuff3 + '@ds011439.mlab.com:11439/heroku_wk47xfd5';
 mongoose.connect(mongoURI);
 
-// morgan for server loggin outside of heroku
-if (!process.env.PORT) {
-  app.use(require('morgan')('dev'))
-}
-
 app.set('port', (process.env.PORT || 5000));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+app.set('view engine', 'html');
 
 app.use(express.static(path.join(__dirname, './..')));
 
