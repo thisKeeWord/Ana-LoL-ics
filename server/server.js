ff959387157d274d4ce85da5069303e286916578
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2016-03-13 22:44:50 -0700

    changes

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ mongoose.connect(mongoURI);
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
-app.set('view engine', 'html');
+// app.set('view engine', 'html');
 
 app.use(express.static(path.join(__dirname, './..')));
 
