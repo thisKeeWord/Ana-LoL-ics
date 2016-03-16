ade9e57053ed9fb8776677eafd8d7f1f6846d86d
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2016-03-16 16:59:11 -0700

    added instructions for user to click

--- client/summRift.jsx
+++ client/summRift.jsx
@@ -9,6 +9,7 @@ class GamesOnSR extends React.Component {
   render() {
     return (
       <div id="matches">
+        click on one of the following: <br/>
         { this.props.res.map(matchList => {
             return (
               <input type="submit" id={matchList[0]} key={matchList[0]} onClick={this.handleClick.bind(this)} style={{backgroundSize: "30px", backgroundImage:"url(" + matchList[1] + ")",  backgroundRepeat: "no-repeat", "height":"40px"}} value={matchList[2]} />
