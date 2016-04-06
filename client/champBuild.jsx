9ff8afd96e48f33b32403754e7fd6ceef70dc5bd
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2016-04-06 00:06:43 -0700

    removed console.log

--- client/champBuild.jsx
+++ client/champBuild.jsx
@@ -119,9 +119,7 @@ class ChampBuild extends React.Component {
     }
   }
 
-  appendItems(showItems) {
-    console.log(this.props.playerInfo1)
- 
+  appendItems(showItems) { 
     // REMOVE CONSTANT CREATIONS OF ICONS AND BUILD IMAGES
     if ((this.props.addItems1 && this.props.gamesToSee === 1) || (this.props.addItems2 && this.props.gamesToSee === 2)) {
       for (let i = 1; i <= this.props.gamesToSee; i++) {
