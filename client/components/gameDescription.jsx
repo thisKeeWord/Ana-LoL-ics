20cc48d4e1c345945a403ac326252ec643a28299
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2017-06-13 22:13:23 -0700

    game description showing but needs work

--- client/components/gameDescription.jsx
+++ client/components/gameDescription.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
 
+
 class GameDescription extends React.Component {
+  getGameDescription() {
+    let rows = [];
+    for (let i = 0; i < this.props.gameSumm.length; i++) {
+      let infoByGame = this.props.gameSumm[i].split('break');
+      let description = [];
+      for (let j = 0; j < infoByGame.length; j++) {
+        let subData = infoByGame[j];
+        description.push(<td key={subData} id={subData}>{subData}</td>)
+      }
+      rows.push(<tr key={i} id={"data" + i}>{description}</tr>);
+    }
+    return rows;
+  }
+  
   render() {
     // console.log(this.props.gameSumm)
     if (this.props.gameSumm) {
+      let gameInfo = this.getGameDescription();
+      console.log('asdfasdf')
       return (
-        <div className="gameDesc">
-          { this.props.gameSumm.map((el, index) => {
-              // console.log(el, 'element')
-              let detailBreakdown = el.split('break');
-              return (
-                <div id={"gameDetails" + index}>
-                  {detailBreakdown[0]}
-                </div>
-              )
-            })
-          }
-
+        <div className="gameDescription">
+          <div className="row">
+            <div className="col s12 board">
+              <table id="simple-board">
+                 <tbody>
+                   {gameInfo}
+                 </tbody>
+               </table>
+            </div>
+          </div>
+          yolo
         </div>
       )
     }
@@ -26,6 +42,7 @@ class GameDescription extends React.Component {
       )
     }
   }
+
 }
 
 module.exports=GameDescription;
