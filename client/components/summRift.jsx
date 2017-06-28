4bc4087acccb8fa3beb55e3bbef50f4783bcaad5
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2017-06-27 21:29:22 -0700

    hoverable to see match history

--- client/components/summRift.jsx
+++ client/components/summRift.jsx
@@ -19,12 +19,17 @@ class GamesOnSR extends React.Component {
           <option value="2">2</option>
         </select> 
         from list: <br/>
-        { this.props.res.map(matchList => {
-            return (
-              <input type="submit" id={matchList[0]} className={"games" + this.props.gamesToSee} name={matchList[3] + "break" + matchList[4] + "break" + matchList[5] + "break" + matchList[6]} key={matchList[0]} onClick={this.handleClick.bind(this)} style={{backgroundSize: "25px", backgroundImage:"url(" + matchList[1] + ")",  backgroundRepeat: "no-repeat", "height":"30px"}} value={matchList[2]} />
-            )
-          })
-        }
+        <div className="dropdown">
+          <span className="hoverHere">Hover Here For Match History</span>
+          <div className="dropdown-content">
+            { this.props.res.map(matchList => {
+                return (
+                  <input type="submit" id={matchList[0]} className={"games" + this.props.gamesToSee} name={matchList[3] + "break" + matchList[4] + "break" + matchList[5] + "break" + matchList[6]} key={matchList[0]} onClick={this.handleClick.bind(this)} style={{backgroundSize: "25px", backgroundImage:"url(" + matchList[1] + ")",  backgroundRepeat: "no-repeat", "height":"30px"}} value={matchList[2]} />
+                )
+              })
+            }
+          </div>
+        </div>
       </div>
     )
   }
