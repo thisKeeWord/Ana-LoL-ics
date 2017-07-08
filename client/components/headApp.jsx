a5de976000ba030dd526ab93bff2416c1379cd27
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2017-07-07 20:32:37 -0700

    updated maintenance styling

--- client/components/headApp.jsx
+++ client/components/headApp.jsx
@@ -684,9 +684,9 @@ class HeadApp extends React.Component {
             </form>
             <br /><br /><br /><br />
 
-            <p id="legalStuff">(Legal Stuff: Ana-LoL-ics isn’t endorsed by Riot Games and doesn’t reflect the views<br />
-            or opinions of Riot Games or anyone officially involved in producing or managing<br />
-            League of Legends.League of Legends and Riot Games are trademarks or<br />
+            <p id="legalStuff">(Legal Stuff: Ana-LoL-ics isn’t endorsed by Riot Games and doesn’t reflect the views
+            or opinions of Riot Games or anyone officially involved in producing or managing
+            League of Legends.League of Legends and Riot Games are trademarks or
             registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.)</p>
           
         </div>
