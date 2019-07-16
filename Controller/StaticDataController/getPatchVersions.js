2ea7771738bb3137324667f01d76a69b0d79d349
Leonard Kee <leonardwkee@gmail.com>
2019-07-16 00:18:28 -0700

    .

--- Controller/StaticDataController/getPatchVersions.js
+++ Controller/StaticDataController/getPatchVersions.js
@@ -3,13 +3,12 @@ const rp = require("request-promise");
 
 const patchNumber = util.promisify(getPatchVersion);
 
-
 async function getPatchVersion(info) {
   let apiVersion = null;
   // request(version, function(error, checkingVersion) {
   // var versionChecks = JSON.parse(checkingVersion.body);
-  let versionChecks = JSON.parse(await rp(version));
-  var splitCheck,
+  const versionChecks = JSON.parse(await rp(version));
+  let splitCheck,
     gamePatch,
     patchVersion = 0;
   // if (error) return console.error(error);
@@ -26,7 +25,8 @@ async function getPatchVersion(info) {
   } else {
     apiVersion = versionChecks[0];
   }
-  return apiVersion;
+  
+return apiVersion;
   // });
 }
 
@@ -43,14 +43,14 @@ async function comparePatchVersions(
   req,
   res
 ) {
-  var date = Date.now();
-  let patchDesired = await patchNumber(info).catch(err => console.error(err)); // Don't forget to catch errors;
+  let date = Date.now();
+  const patchDesired = await patchNumber(info).catch((err) => console.error(err)); // Don't forget to catch errors;
   request(`http://ddragon.leagueoflegends.com/cdn/${patchDesired}/data/en_US/item.json`, function(
     err,
     data
   ) {
-    if (error) return console.error(error);
-    var resData = JSON.parse(data.body).data;
+    if (error) {return console.error(error);}
+    let resData = JSON.parse(data.body).data;
 
     // getting timeline information by frame from another endpoint
     // since the first timeline request doesn't have the full info
@@ -58,29 +58,29 @@ async function comparePatchVersions(
     request(
       `https://${regionName}${matchTimelineUrl}${Object.keys(req.body)[0]}?${process.env.stuff1}`,
       function(er, timelineData) {
-        var timelineDataFrames = JSON.parse(timelineData.body).frames;
-        for (var j = 0; j < timelineDataFrames.length; j++) {
+        let timelineDataFrames = JSON.parse(timelineData.body).frames;
+        for (let j = 0; j < timelineDataFrames.length; j++) {
           gameTimeline.push([timelineDataFrames[j]]);
         }
 
         // have to use number for numerator since
         // scroll not up yet
-        var stepScroll = 300 / gameTimeline.length;
+        let stepScroll = 300 / gameTimeline.length;
 
         // going to add this call to db to "cache"
         StaticData.find().exec(function(error, staticInfo) {
-          if (error) return console.error(error);
+          if (error) {return console.error(error);}
           if (!staticInfo || date - staticInfo.created_at >= 604800000) {
             StaticData.remove({}, function(error, removed) {
-              if (error) return console.error(error);
+              if (error) {return console.error(error);}
               request(
                 `http://ddragon.leagueoflegends.com/cdn/${patchDesired}/data/en_US/champion.json`,
                 function(errors, inform) {
-                  var parsedStaticData = JSON.parse(inform.body);
+                  let parsedStaticData = JSON.parse(inform.body);
                   StaticData.create(
                     { created_at: { $lt: date }, static: parsedStaticData },
                     function(err, successful) {
-                      var allChamps = parsedStaticData.data;
+                      let allChamps = parsedStaticData.data;
                       championImageHelper(
                         timelineDataFrames,
                         allChamps,
@@ -101,7 +101,7 @@ async function comparePatchVersions(
               );
             });
           } else {
-            var allChamps = staticInfo[0].static;
+            let allChamps = staticInfo[0].static;
             championImageHelper(
               timelineDataFrames,
               allChamps,
@@ -122,3 +122,4 @@ async function comparePatchVersions(
     );
   });
 }
+
